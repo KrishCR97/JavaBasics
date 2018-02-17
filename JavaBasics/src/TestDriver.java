8dc2ed0041d1df5e2c4d58c9ccc9161cf4b61e7c
KrishCR97 <veerakishon.mucherla@gmail.com>
2018-02-16 19:05:43 -0600

    Initial commit

---
+++ JavaBasics/src/TestDriver.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author GameS
+ */
+public class TestDriver {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //Initially the written java code gets converted to java byte code when 
+        //.java file is compiled and it is portable on all machines.
+        //Then interpreted transalates this byte code to machine understandable 
+        //code and executes the code.
+        
+        //javac javafileName.java -> .class file is generated.
+        //java javaFileName -> converts .class file and machine executes it.
+        System.out.println("Hello");
+    }
+    
+}
