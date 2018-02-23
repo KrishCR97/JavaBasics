d1686ca369b5980d1051c3820c8e8a43f45364f1
Veera Kishon Kumar Mucherla <s528751@nwmissouri.edu>
2018-02-23 13:23:16 -0600

    Added new project JavaW01

---
+++ JavaW01/src/javaw01/Dog.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaw01;
+
+/**
+ *
+ * @author s528751
+ */
+public class Dog {
+    private String name;
+    private String breed;
+    private int age;
+
+    public Dog(String name, String breed, int age) {
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBreed() {
+        return breed;
+    }
+
+    public void setBreed(String breed) {
+        this.breed = breed;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String increementAge(){
+    this.age = this.age + 1;
+    return "Happy birthday " + this.getName();
+    }
+}
---
+++ JavaW01/src/javaw01/JavaW01.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaw01;
+
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author s528751
+ */
+public class JavaW01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello World!!");
+        System.out.println("Hai Cody");
+        //JVM - Interpreter
+        //Compiler -> translates hihlevel code to java byte code.
+        //javac filename.java -> to compile a java program  - .class file extension
+        //java filename -> to execute a java program
+        
+        //An object reperesents both state-Members and behavior-Methods.
+        Dog d = new Dog("Jimmy", "Pamperian", 7);
+        System.out.println(d.getName());
+        System.out.println(d.increementAge());
+        //out -- is a printstream object
+        Rectangle rect = new Rectangle(12, 6);
+        System.out.println(rect.getHeight());
+        
+    }
+    
+}
